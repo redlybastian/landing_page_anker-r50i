9cd672a14b661605606182e6f97ff9dca34fb82d
redlybastian <red233786@gmail.com>
2026-02-24 15:12:24 +0530

    Add hamburger icon image to public directory

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,31 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <h1 className="text-center">Home</h1>
+   <header>
+    <nav>
+      <img className="w-6 m-4" sizes="20px" src="hamburger.png" alt="nav-button" />      
+    </nav>
+   </header>
+   <main>
+    {/* banner section */}
+    <section className="">
+      <h1 className="text-center mt-10 ml-10 mr-10 mb-3 text-4xl font-bold"><span className="text-[#0986FA]">Anker</span> Soundcore <span className="text-[#18A7FF]">R50i NC</span></h1>
+      <h2 className="text-center  text-3xl font-bold">Silence the <span className="text-[#0986FA]">Noise</span>,<br /><span className="bg-gradient-to-r from-[#1A688F] to-[#F61DEF] bg-clip-text text-transparent">Power</span> Your Sound.</h2>
+      <div className="flex justify-center mt-10">
+        <Image src={'/Anker-R50i-NC-white_img1-removebg.png'}
+        width={320}
+        height={300}
+        alt="Anker-R50i-NC-white-colombo" 
+        priority
+         />
+      
+      </div>
+      <div className="flex justify-center mt-5">
+         <button className="m-5 pt-5 pb-5 pl-7 pr-7 bg-black rounded-2xl text-white text-xs font-bold cursor-pointer">BUY NOW</button>
+      </div>
+      
+    </section>
+   </main>
     </>
   );
 }
